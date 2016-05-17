d88d0f343361814a80ea136fa768abe0bcf637c2
jdrennen <jdrennen@brittenford.com>
2016-05-17 15:02:39 -0400

    Scope-Chains-Closurs: 4. Closures

--- src/scope-chains-closures/scopes.js
+++ src/scope-chains-closures/scopes.js
@@ -1,10 +1,13 @@
 // Scope-Chains-Closurs: 1. Scopes
 // Scope-Chains-Closurs: 2. Scope Chains
 // Scope-Chains-Closurs: 3. Global Scope & Shadowing
+// Scope-Chains-Closures: 4. Closures
 function foo() {
 	var bar;
 	quux = "Global scope";
 	function zip() {
 		var quux = "Lexical";
+		bar = true;
 	}
+	return zip;
 }
