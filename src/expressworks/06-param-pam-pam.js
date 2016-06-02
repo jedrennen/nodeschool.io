35e7891be8bb7c10d736715db1a70946bb2d4fcb
Justin Drennen <justin@drennentech.com>
2016-06-02 09:52:46 -0400

    ExpressWorks: 06-PARAM PAM PAM

---
+++ src/expressworks/06-param-pam-pam.js
@@ -0,0 +1,14 @@
+// ExpressWorks: 06-PARAM PAM PAM
+var express = require('express'),
+	crypto = require('crypto');
+
+var app = express();
+
+app.put('/message/:id', function(req, res) {
+	var id = req.params.id;
+	res.end(crypto.createHash('sha1')
+		.update(new Date().toDateString() + id)
+		.digest('hex'));
+});
+
+app.listen(process.argv[2]);
