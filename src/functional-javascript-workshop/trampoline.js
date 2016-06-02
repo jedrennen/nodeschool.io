8b94f61d4e244587a5e7f82b587f4b1f99aaacc3
Justin Drennen <justin@drennentech.com>
2016-06-01 23:19:01 -0400

    Functional Javascript Workshop: 14-Trampoline

---
+++ src/functional-javascript-workshop/trampoline.js
@@ -0,0 +1,20 @@
+// Functional Javascript Workshop: 14-Trampoline
+function repeat(operation, num) {
+    return function() {
+        if (num <= 0) return;
+        operation();
+        return repeat(operation, --num);
+    }
+}
+
+function trampoline(fn) {
+    while (fn && typeof fn === 'function') {
+        fn = fn();
+    }
+}
+
+module.exports = function(operation, num) {
+    trampoline(function() {
+        return repeat(operation, num);
+    });
+}
